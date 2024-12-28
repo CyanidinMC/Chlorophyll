48c661a31d0edd287deeb367ab9e90e37abefa8c
MrHua269 <wangxyper@163.com>
2024-12-28 23:03:02 +0800

    Rewrote connection ticking on Server Thread side

--- src/main/java/me/mrhua269/chlorophyll/mixins/network/ServerConnectionListenerMixin.java
+++ src/main/java/me/mrhua269/chlorophyll/mixins/network/ServerConnectionListenerMixin.java
@@ -1,25 +1,73 @@
 package me.mrhua269.chlorophyll.mixins.network;
 
 import net.minecraft.network.Connection;
+import net.minecraft.network.PacketSendListener;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.common.ClientboundDisconnectPacket;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerConfigurationPacketListenerImpl;
 import net.minecraft.server.network.ServerConnectionListener;
 import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 import net.minecraft.server.network.ServerStatusPacketListenerImpl;
-import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Iterator;
+import java.util.List;
 
 @Mixin(ServerConnectionListener.class)
 public class ServerConnectionListenerMixin {
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Connection;tick()V"))
-    public void redirect$TickCheck(@NotNull Connection instance){
-        boolean shouldTick = instance.getPacketListener() instanceof ServerLoginPacketListenerImpl
-                || instance.getPacketListener() instanceof ServerStatusPacketListenerImpl
-                || instance.getPacketListener() instanceof ServerConfigurationPacketListenerImpl;
+    @Shadow @Final
+    List<Connection> connections;
 
-        if (shouldTick) {
-            instance.tick();
+    @Shadow @Final private static Logger LOGGER;
+
+    @Shadow @Final
+    MinecraftServer server;
+
+    /**
+     * @author MrHua269
+     * @reason Worldized ticking
+     */
+    @Overwrite
+    public void tick() {
+        synchronized(this.connections) {
+            Iterator<Connection> iterator = this.connections.iterator();
+
+            while (iterator.hasNext()) {
+                final Connection connection = iterator.next();
+
+                if (connection.isConnecting()) {
+                    continue;
+                }
+
+                boolean shouldTick = connection.getPacketListener() instanceof ServerLoginPacketListenerImpl
+                        || connection.getPacketListener() instanceof ServerStatusPacketListenerImpl
+                        || connection.getPacketListener() instanceof ServerConfigurationPacketListenerImpl;
+
+                if (!shouldTick) {
+                    continue;
+                }
+
+                if (connection.isConnected()) {
+                    try {
+                        connection.tick();
+                    } catch (Exception ex) {
+                        LOGGER.warn("Failed to handle packet for {}", connection.getLoggableAddress(this.server.logIPs()), ex);
+                        final Component component = Component.literal("Internal server error");
+
+                        connection.send(new ClientboundDisconnectPacket(component), PacketSendListener.thenRun(() -> connection.disconnect(component)));
+                        connection.setReadOnly();
+                    }
+                } else {
+                    iterator.remove();
+                    connection.handleDisconnection();
+                }
+            }
         }
     }
+
 }
